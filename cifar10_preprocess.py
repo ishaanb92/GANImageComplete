REALLY fixing the normalization bug

cifar10_preprocess.py
@@ -9,7 +9,6 @@ from sklearn.preprocessing import MinMaxScaler
 def generate_dataset():
     ds = cifar10.CIFAR10(n_val=0)
     dataset = np.transpose(ds.train_X,(0,2,3,1))
-    print(dataset.shape)
     return dataset
 
 def generate_batch(dataset,batch_size,shuffle = True):
@@ -21,11 +20,12 @@ def generate_batch(dataset,batch_size,shuffle = True):
         np.random.shuffle(dataset)
     idx = np.random.randint(dataset.shape[0],size=batch_size)
     batch = dataset[idx,:,:,:]
-    # Normalize
+    normalized_batch = []
+    # Images must be moved from [0,1] -> [-1,1]
     for image in batch:
-        image = (image/127.5) - 1
-
-    return batch
+        norm_image = np.array(image)/0.5 - 1.
+        normalized_batch.append(norm_image)
+    return np.asarray(normalized_batch)
 
 if __name__ == '__main__':
     dataset = generate_dataset()

utils.py
@@ -65,7 +65,7 @@ def transform(image, npx=64, is_crop=True):
     return np.array(cropped_image)/127.5 - 1.
 
 def inverse_transform(images):
-    return (images+1.)/2.
+    return (images + 1.)/2.
 
 
 def to_json(output_path, *layers):