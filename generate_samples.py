7aba55f4455a582f6d8de09aa0adc1d1b6c05430
kilgore92 <ishaan.bits92@gmail.com>
2017-10-16 12:48:24 +0200

    Adding GPU binding to generate API

--- generate_samples.py
+++ generate_samples.py
@@ -13,6 +13,10 @@ from model import DCGAN
 from utils import visualize
 import numpy as np
 
+# Use GPU1
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
@@ -21,8 +25,8 @@ flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size",64, "The size of batch images [64]")
 flags.DEFINE_integer("image_size",64 , "The size of image to use")
 flags.DEFINE_string("dataset", "lfw-aligned-64", "Dataset directory.")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("sample_dir", "samples_lsun_test", "Directory name to save the image samples [samples]")
+flags.DEFINE_string("checkpoint_dir", "checkpoint_lsun_64", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("sample_dir", "samples_lsun_64_test", "Directory name to save the image samples [samples]")
 flags.DEFINE_string("num_batches",50, "Number of batches to generate")
 FLAGS = flags.FLAGS
 
